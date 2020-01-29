fa6d009ed213c9137ecea1df6ea00dcd0a68ed76
Eugene Marinelli <eugene@blend.com>
2020-01-28 18:08:16 -0800

    parsing

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,23 @@
 import * as puppeteer from 'puppeteer'
 import * as _ from 'lodash'
 
+interface Workout {
+  id: number
+  due: string
+  workout_item_ids: number[]
+}
+
+interface WorkoutItem {
+  name: string
+  info: string
+  workout_id: number
+}
+
+interface WorkoutsResponse {
+  workouts: Workout[]
+  workout_items: WorkoutItem[]
+}
+
 (async () => {
   const browser = await puppeteer.launch({ headless: false })
   const page = await browser.newPage()
@@ -12,12 +29,21 @@ import * as _ from 'lodash'
 
   page.on('response', async response => {
     if (response.url().indexOf('proxy/api/clients/159147/workouts') !== -1) {
-      const responseText = await response.text()
-      console.log(responseText);
-      const workouts = JSON.parse(responseText)
-      console.log(workouts);
+      const workoutsResponse = JSON.parse(await response.text()) as WorkoutsResponse
+      const workouts = workoutsResponse.workouts
+      const workoutItems = workoutsResponse.workout_items
 
-      _.each(workouts)
+      _.each(workouts, (workout: Workout) => {
+        console.log(workout.id);
+        console.log(workout.due);
+        console.log(workout.workout_item_ids);
+      })
+
+      _.each(workoutItems, (workoutItem: WorkoutItem) => {
+        console.log(workoutItem.workout_id);
+        console.log(workoutItem.name);
+        console.log(workoutItem.info);
+      })
     }
   })
 
