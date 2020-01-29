e4bf4d69fcd95993d99faa69024c7b58c2c7b5f7
Eugene Marinelli <eugene@blend.com>
2020-01-28 21:15:32 -0800

    cleanup

--- src/workoutItemParser.ts
+++ src/workoutItemParser.ts
@@ -21,18 +21,19 @@ export function infoToSets(date: string, exercise: string, info: string): Workou
 
   return _.flatMap(lines, l => {
     const xCount = (l.match(/x/g) || []).length
-    if (xCount === 1) {
+    if (xCount === 0) {
+      // Ex: 8@9
+      const { reps, rpe } = parseRepsAndRpe(l)
+      return [{ date, exercise, reps, rpe }]
+    } else if (xCount === 1) {
       // Ex: 8@9 x 2 sets
       const [repsAndRpe, setsStr] = _(l).split('x').map(s => s.trim()).value()
       const { reps, rpe } = parseRepsAndRpe(repsAndRpe)
 
       const numSets = parseNumSets(setsStr)
-      return _.map(_.range(numSets), _i => ({
-        date,
-        exercise,
-        rpe,
-        reps
-      }))
+      return _.map(_.range(numSets), _i => (
+        { date, exercise, rpe, reps }
+      ))
     } else if (xCount === 2) {
       // Ex: -5% x 4 reps x 2 sets
       const [rpe, repsStr, setsStr] =
@@ -40,23 +41,11 @@ export function infoToSets(date: string, exercise: string, info: string): Workou
 
       const reps = parseInt(repsStr.split(' ')[0])
       const numSets = parseNumSets(setsStr)
-
-      return _.map(_.range(numSets), _i => ({
-        date,
-        exercise,
-        rpe,
-        reps
-      }))
+      return _.map(_.range(numSets), _i => (
+        { date, exercise, rpe, reps }
+      ))
     } else {
-      // Ex: 8@9
-      const { reps, rpe } = parseRepsAndRpe(l)
-      return [{
-        date,
-        exercise,
-
-        reps,
-        rpe,
-      }]
+      throw new Error('unhandled')
     }
   })
 }
