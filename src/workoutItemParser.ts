daf276a2264f590e7aa820da63b437c553661217
Eugene Marinelli <eugene@blend.com>
2020-01-28 21:01:06 -0800

    fix more stuff

--- src/workoutItemParser.ts
+++ src/workoutItemParser.ts
@@ -1,35 +1,62 @@
 import * as _ from 'lodash'
 
+// Ex: 8@9
+function parseRepsAndRpe(s: string) {
+  const [reps, rpe] = s.split('@')
+  return {
+    reps: parseInt(reps),
+    rpe
+  }
+}
+
+// Ex: "2 sets"
+function parseNumSets(s: string) {
+  return parseInt(s.split(' ')[0])
+}
+
 export function infoToSets(date: string, exercise: string, info: string): WorkoutSet[] {
   const lines = _(info).split('\n')
     .map(l => l.replace('•', '').trim())
     .value()
 
-  const sets = _(lines).slice(0, lines.length - 1).map(l => {
-    const [reps, rpe] = l.split('@')
-    return {
-      date,
-      exercise,
-      reps: parseInt(reps),
-      rpe,
+  return _.flatMap(lines, l => {
+    const xCount = (l.match(/x/g) || []).length
+    if (xCount === 1) {
+      // Ex: 8@9 x 2 sets
+      const [repsAndRpe, setsStr] = _(l).split('x').map(s => s.trim()).value()
+      const { reps, rpe } = parseRepsAndRpe(repsAndRpe)
+
+      const numSets = parseNumSets(setsStr)
+      return _.map(_.range(numSets), _i => ({
+        date,
+        exercise,
+        rpe,
+        reps
+      }))
+    } else if (xCount === 2) {
+      // Ex: -5% x 4 reps x 2 sets
+      const [rpe, repsStr, setsStr] =
+        _(l).split('x').map(s => s.trim()).value()
+
+      const reps = parseInt(repsStr.split(' ')[0])
+      const numSets = parseNumSets(setsStr)
+
+      return _.map(_.range(numSets), _i => ({
+        date,
+        exercise,
+        rpe,
+        reps
+      }))
+    } else {
+      // Ex: 8@9
+      const { reps, rpe } = parseRepsAndRpe(l)
+      return [{
+        date,
+        exercise,
+
+        reps,
+        rpe,
+      }]
     }
-  }).value()
-
-  const backoffsLine = lines[lines.length - 1]
-
-  // Ex: -5% x 4 reps x 2 sets
-  const [rpe, repsStr, setsStr] =
-    _(backoffsLine).split('x').map(s => s.trim()).value()
-
-  const reps = parseInt(repsStr.split(' ')[0])
-  const numSets = parseInt(setsStr.split(' ')[0])
-
-  const backoffSets = _.map(_.range(numSets), _i => ({
-    date,
-    exercise,
-    rpe,
-    reps
-  }))
-
-  return sets.concat(backoffSets)
+  })
 }
--- test/workoutItemParser.test.ts
+++ test/workoutItemParser.test.ts
@@ -5,7 +5,7 @@ const test = anyTest as TestInterface<{}>
 
 import * as workoutItemParser from '../src/workoutItemParser'
 
-test('yo', t => {
+test('backoffs - -5% x 4 reps x 2 sets', t => {
   const date = '2020-01-28'
   const exercise = 'deadlifts'
 
@@ -21,5 +21,40 @@ test('yo', t => {
     { date, exercise, reps: 4, rpe: '8' },
     { date, exercise, reps: 4, rpe: '-5%' },
     { date, exercise, reps: 4, rpe: '-5%' },
+  ]))
+})
+
+test('backoffs - 8@9 x 2 sets', t => {
+  const date = '2020-01-30'
+  const exercise = 'belt squat'
+
+  const sets = workoutItemParser.infoToSets(
+    date,
+    exercise,
+    '• 8@7\n• 8@8\n• 8@9 x 2 sets',
+  )
+
+  t.true(_.isEqual(sets, [
+    { date, exercise, reps: 8, rpe: '7' },
+    { date, exercise, reps: 8, rpe: '8' },
+    { date, exercise, reps: 8, rpe: '9' },
+    { date, exercise, reps: 8, rpe: '9' },
+  ]))
+})
+
+test('no backoffs', t => {
+  const date = '2020-01-30'
+  const exercise = 'belt squat'
+
+  const sets = workoutItemParser.infoToSets(
+    date,
+    exercise,
+    '• 8@7\n• 8@8\n• 8@9',
+  )
+
+  t.true(_.isEqual(sets, [
+    { date, exercise, reps: 8, rpe: '7' },
+    { date, exercise, reps: 8, rpe: '8' },
+    { date, exercise, reps: 8, rpe: '9' },
   ]))
 })
