6f6f545c602369f084a6837a85954fd61ec84a4a
Eugene Marinelli <eugene@blend.com>
2020-11-17 08:55:26 -0600

    handle same weight sets

--- test/workoutItemParser.test.ts
+++ test/workoutItemParser.test.ts
@@ -190,3 +190,31 @@ test('handles rep range', t => {
     reps: '15-20'
   }]))
 })
+
+test('handles sets at same weight', t => {
+  const date = '2020-11-16'
+  const exercise = 'High Bar Squat'
+
+  const sets = workoutItemParser.infoToSets(
+    date,
+    exercise,
+    '5 reps x 3 sets w/ same weight'
+  )
+
+  t.true(_.isEqual(sets, [{
+    date,
+    exercise,
+    rpe: 'same weight',
+    reps: '5'
+  }, {
+    date,
+    exercise,
+    rpe: 'same weight',
+    reps: '5'
+  }, {
+    date,
+    exercise,
+    rpe: 'same weight',
+    reps: '5'
+  }]))
+})
